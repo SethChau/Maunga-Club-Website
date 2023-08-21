8c259059a12678d189d5c369c195eb7c789b1c32
SethChau <132409422+SethChau@users.noreply.github.com>
2023-08-22 09:40:00 +1200

    index.html.py

---
+++ Internal.py
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css">
+    <title>Maunga Club - Home</title>
+</head>
+
+<body class="home">
+    <header>
+        <a href="index.html"><img class="logo" src="Resources/logo.png" alt="Maunga Club logo" /></a>
+        <nav>
+            <a href="Index.html">Home</a>
+            <a href="kakapo.html">Kākāpo</a>
+            <a href="pukeko.html">Pūkeko</a>
+            <a href="kereru.html">Kererū</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+    </header>
+    <div class="hero-content">
+        <h1>Welcome to Maunga Club</h1>
+        <p>Here you can enjoy the experience of a snowboarding and skiing trip with the comfort of chalets. In Maunga Club, we offer 3 different types of chalets of different sizes. Kākāpo is the largest, Pūkeko is medium-sized, and Kererū is the smallest.</p>
+        <ul>
+            <li>Our chalets are only open to Maunga club members</li>
+            <li>All chalet bookings must be from Friday to Sunday</li>
+            <li>Chalets offer shared accommodation for between 6 and 40 people per chalet</li>
+            <li>All three chalets are on the mountain, so you can ski to and from the chalets</li>
+            <li>The mountain provides 550 hectares of skiing/snowboarding area, and the longest ski/snowboard season in New Zealand</li>
+        </ul>
+    </div>
+    <footer>
+        <p><small>&copy; Maunga Club 2023 - Website by Seth Chau</small></p>
+    </footer>
+</body>
+
+</html>
